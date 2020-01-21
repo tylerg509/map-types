73bcf51b0f3c0f40c806156fa0a9c58cacef552a
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-21 05:24:42 -0800

    define lat lng

--- src/User.ts
+++ src/User.ts
@@ -10,6 +10,10 @@ class User{
     }
 
     constructor(){
-        
+        this.name = faker.name.firstName(); 
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
     }
 }
