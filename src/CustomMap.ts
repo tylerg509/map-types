83c02e4c571d82047040d1ad49490a9d0542b7fc
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-21 06:17:12 -0800

    use private modifier to restrict access to googlemap

---
+++ src/CustomMap.ts
@@ -0,0 +1,13 @@
+export class CustomMap{
+    //first use of private to limit developer access to googlemap
+    private googleMap: google.maps.Map;
+    constructor(divId: string){
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        })
+    }
+}
