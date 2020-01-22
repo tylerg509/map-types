caf3e425d1aba15274bffdd5b73524a048680149
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-21 23:14:57 -0800

    finish implementing same addmarker for two objects

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,8 @@ const user = new User();
 const company = new Company()
 const customMap = new CustomMap('map');
 
+//note that we use addmarker for user and company
+//the interface on addmarker accepts any arguments
+//the have location: lat and lng
 customMap.addMarker(user)
-customMap.addCompanyMarker(company)
+customMap.addMarker(company)
